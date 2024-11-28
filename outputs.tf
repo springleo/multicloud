5016e3a02d49d86d47be1aee9ad46080b0b8694d
springleo <mikkilimanohar@gmail.com>
2024-11-28 21:27:38 +0530

    conditionalize output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,8 @@
 output "s3_bucket_url" {
-  value = "https://${module.aws_s3.s3_bucket_name}.s3.amazonaws.com"
+  #value = "https://${module.aws_s3.s3_bucket_name}.s3.amazonaws.com"
+  value = var.is_aws_enabled ? "https://${module.aws_s3.s3_bucket_name}.s3.amazonaws.com" : "No AWS S3 bucket created"
 }
 
 output "azure_storage_account_url" {
-  value = module.azure_storage.storage_account_name
+  value = var.is_azure_enabled ? module.azure_storage.storage_account_name : "No Azure storage account created"
 }
