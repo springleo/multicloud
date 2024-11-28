9bee4e99c73d9cad59c93df7d2cf1cfdd49307e9
springleo <mikkilimanohar@gmail.com>
2024-11-28 21:46:39 +0530

    use index again

--- azure/storage_account.tf
+++ azure/storage_account.tf
@@ -7,8 +7,8 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_storage_account" "storage" {
   count = var.is_azure_enabled == true ? 1 : 0
   name                     = var.storage_name
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg[0].name
+  location                 = azurerm_resource_group.rg[0].location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
